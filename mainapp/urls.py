d3d65228c51ef3bd980c3e9304ef3b4bbf8316c3
Alexey Ryzhkov <aler32@mail.ru>
2022-11-28 23:09:33 +0300

    Lesson_3 Full

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.generic.base import RedirectView
 
 from mainapp import views
 from mainapp.apps import MainappConfig
@@ -8,6 +9,7 @@ app_name = MainappConfig.name
 urlpatterns = [
     path("", views.MainPageView.as_view(), name="main_page"),
     path("news/", views.NewsPageView.as_view(), name="news"),
+    path("news/<str:params>/", RedirectView.as_view(url="https://google.com/search?q=%(params)s")),
     path("news/<int:page>/", views.NewsWithPaginatorView.as_view(), name="news_paginator"),
     path("courses/", views.CoursesPageView.as_view(), name="courses"),
     path("contacts/", views.ContactsPageView.as_view(), name="contacts"),
